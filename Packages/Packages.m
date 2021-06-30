a253777761fa0fce1aa00f1ccf01a636f4e3a5d2
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-06-30 03:41:09 +0300

    Add files via upload

---
+++ Packages/Packages.m
@@ -0,0 +1,3 @@
+pkg list %in order to see which packages are created (octave libraries we use this command)
+
+pkg load <packname> %we choose which package we want to load example pkg load communications
