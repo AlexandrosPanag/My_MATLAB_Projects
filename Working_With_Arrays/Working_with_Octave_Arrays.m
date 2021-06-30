aec9c3bf00f0e7452037757283ad9ce7fd7f7c80
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-06-30 03:28:26 +0300

    Add files via upload

---
+++ Working_With_Arrays/Working_with_Octave_Arrays.m
@@ -0,0 +1,6 @@
+A = [ 1, 1, 2; 3, 5, 8; 13, 21, 34 ]  #making an array
+#with 1,2,3; we switch the array line
+# A = [ 1, 1, 2; 3, 5, 8; 13, 21, 34 ]; printing an array
+
+B = rand(3,4); %With this way we can create an array with random values up to the number one
+#
