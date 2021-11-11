8e481dc0303691ec69f4252a12b75fed2a949fb2
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-11-11 18:26:03 +0200

    Add files via upload

---
+++ Indexes (Arrays)/Finding the Average.m
@@ -0,0 +1,5 @@
+A = [3, 0, -5, 2; 7, 12, 4, 1; -1, 3, 7, 0] %Creating the array
+M = mean(A) %finding the Average number
+
+formatIntToFloat = '%8.1f \n'; %Making the Integer value into a float number
+fprintf(formatIntoToFloat,mean(A)) %printing the float number with the mean
