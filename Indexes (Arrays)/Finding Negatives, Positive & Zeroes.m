8677ce20b046838676c4b1a0e1ae2db7fafecbac
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2022-11-04 10:55:21 +0200

    Update Finding Negatives, Positive & Zeroes.m

--- Indexes (Arrays)/Finding Negatives, Positive & Zeroes.m
+++ Indexes (Arrays)/Finding Negatives, Positive & Zeroes.m
@@ -1,3 +1,7 @@
+%
+% By Alexandros Panagiotakopoulos
+%
+
 function calc = calculation(A)
   A = [-1,-2,-3,-4,-5,-6,0,1,2,3,4,5,6,7,8,9,10]
   s=sign(A) %we give the array a sign to determine the positions
