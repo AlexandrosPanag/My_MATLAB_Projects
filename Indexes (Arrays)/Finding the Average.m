900ec53d1c57c98aa094bf5b12836a3fe7619132
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2022-11-15 18:11:51 +0200

    Delete Indexes (Arrays) directory

--- Indexes (Arrays)/Finding the Average.m
+++
@@ -1,9 +0,0 @@
-%
-% By Alexandros Panagiotakopoulos
-%
-
-A = [3, 0, -5, 2; 7, 12, 4, 1; -1, 3, 7, 0] %Creating the array
-M = mean(A) %finding the Average number
-
-formatIntToFloat = '%8.1f \n'; %Making the Integer value into a float number
-fprintf(formatIntoToFloat,mean(A)) %printing the float number with the mean
--- Indexes (Arrays)/Random_Array.m
+++
@@ -1,7 +0,0 @@
-%
-% By Alexandros Panagiotakopoulos
-%
-
-r = rand(3)   %creating a random array 3x3 (1-Dimensional)
-r = rand(3,3) %creating a random array 3x3 (2-Dimensional)
-
