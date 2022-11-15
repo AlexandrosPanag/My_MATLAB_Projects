c9e8e4593348b4b301df1fd66022a9a2224a99e6
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2022-11-15 18:16:24 +0200

    Add files via upload

---
+++ Matrix Indexing/Finding_Max.m
@@ -0,0 +1,19 @@
+# By Alexandros Panagiotakopoulos
+
+clc; clear;
+
+%creating a random matrix
+max=10; % creating an array with 10 elements
+x=[];
+for i=1:max
+x(i)=randi([-9 9])
+end
+
+
+%finding the subarray using BRUTE FORCE
+ maxval = x(1);
+ for ii = 1:length(x)
+    if x(ii) > maxval
+        maxval = x(ii);
+    end
+ end
