700245a43cd1b81f325912e42e0c720f789f75ca
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2023-01-03 10:51:06 +0200

    Delete Packages directory

--- Packages/Packages.m
+++
@@ -1,3 +0,0 @@
-pkg list %in order to see which packages are created (octave libraries we use this command)
-
-pkg load <packname> %we choose which package we want to load example pkg load communications
--- Packages/README.md
+++
@@ -1,4 +0,0 @@
-Packages in Octave (libraries)
------
-
-This is a basic example of how we can load and test specific packages in octave (commonly used term in programmming libraries) .
